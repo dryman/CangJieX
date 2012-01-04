8601569a0d5d69fefdfaef58283f5ca920218098
dryman <idryman@gmail.com>
2012-01-04 11:10:01 +0800

    add missing DDFileReader

---
+++ DDFileReader.h
@@ -0,0 +1,36 @@
+//
+//  DDFileReader.h
+//  NSCoderTest
+//
+//  Created by Felix Ren-Chyan Chern on 11/12/17.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+@interface DDFileReader : NSObject {
+    NSString * filePath;
+    
+    NSFileHandle * fileHandle;
+    unsigned long long currentOffset;
+    unsigned long long totalFileLength;
+    
+    NSString * lineDelimiter;
+    NSUInteger chunkSize;
+}
+
+@property (nonatomic, copy) NSString * lineDelimiter;
+@property (nonatomic) NSUInteger chunkSize;
+
+- (id) initWithFilePath:(NSString *)aPath;
+
+- (NSString *) readLine;
+- (NSString *) readChompedLine;
+- (NSString *) readTrimmedLine;
+
+#if NS_BLOCKS_AVAILABLE
+- (void) enumerateLinesUsingBlock:(void(^)(NSString*, BOOL *))block;
+#endif
+
+@end

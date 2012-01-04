34be2d9a95b127fe911ea78da70ce6e828472784
dryman <idryman@gmail.com>
2012-01-04 17:26:33 +0800

    fix analysis warnings

--- CangJieX/main.m
+++ CangJieX/main.m
@@ -14,11 +14,9 @@ IMKServer* server;
 
 int main(int argc, char *argv[])
 {
-    NSString*       identifier;
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 	
 	//find the bundle identifier and then initialize the input method server
-    identifier = [[NSBundle mainBundle] bundleIdentifier];
     server = [[IMKServer alloc] initWithName:(NSString*)connectionName bundleIdentifier:[[NSBundle mainBundle] bundleIdentifier]];
     NSArray *arr = [NSConnection allConnections];
     for (NSConnection* connection in arr){
